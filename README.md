e28d99bc455cf65b4694684bbf92e6101d3081ac
Adam Wulf <adam.wulf@gmail.com>
2021-05-12 17:38:58 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rationals
 
-A description of this package.
+A description of this package. Based on Noah Wilder's Fraction package at https://github.com/Wildchild9/SwiftFractions
--- Tests/RationalsTests/RationalsTests.swift
+++ Tests/RationalsTests/RationalsTests.swift
@@ -2,10 +2,5 @@
     @testable import Rationals
 
     final class RationalsTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(Rationals().text, "Hello, World!")
-        }
+        
     }
